89a40bddda6384cc1d28771adaef2ec96f0a94e3
hengyu <hengyu@outlook.com>
2023-03-31 13:45:20 +0800

    Update LICENSE year

--- Sources/PasscodeField/PasscodeField.swift
+++ Sources/PasscodeField/PasscodeField.swift
@@ -1,7 +1,7 @@
 //
 // MIT License
 //
-// Copyright (c) 2022 hengyu
+// Copyright (c) 2022-2023 hengyu
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
